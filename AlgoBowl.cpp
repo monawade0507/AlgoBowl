f3f38a254585f90548961494c0868c75ec9c714b
Mona Wade <dwade@mines.edu>
2019-10-11 22:23:43 -0600

    Working with limitations

--- AlgoBowl.cpp
+++ AlgoBowl.cpp
@@ -6,12 +6,19 @@
 #include <map>
 #include <bits/stdc++.h>
 #include <set>
+#include <iostream>
+#include <math.h>
 
 using namespace std;
 
 set<string> covers;
+char firstRepeating(string &str);
+bool printDuplicates(vector<string> words);
 void findDec(int numSets);
 void findInc(int numSets, int end);
+//vector<int> findPowerSet(vector<int> const &S, vector<int> &set, int n);
+vector<string> printPowerSet(vector<int> __set, int set_size);
+void findPowerSet(vector<int>S, vector<int> set, int n);
 template<typename Out>
 void split(const std::string &s, char delim, Out result);
 vector<string> split(const string &s, char delim);
@@ -76,37 +83,35 @@ int main(int argc, char const *argv[]) {
   // check to see if contents were store
   cout << "Amount of SubSets: " << _set.size() << endl;
 
+// *************************************************************************************************************************************
+// FINISHED ABOVE
+// *************************************************************************************************************************************
+
 
   //the set cover problem goal is to identify the smallest
   //sub-collection of S whose union equals the universe
 
-  string _cover = "";
+
+  vector<int> S;
+  vector<int> setA;
+
   for(int i = 0; i < numSubSets; i++) {
-    string num = to_string(i);
-    _cover += num + " ";
-    covers.insert(_cover);
-  }
-  _cover = "";
-  for(int i = numSubSets - 1; i > 0; i--) {
-    string num = to_string(i);
-    _cover += num + " ";
-    covers.insert(_cover);
+    S.push_back(i);
   }
 
-  // find covers using recursion
-  findDec(numSubSets);
-  findInc(0, numSubSets);
+  int n = S.size();
+  cout << "FindPowerSet with amount: " << S.size() << endl;
 
-  // check to see the amount of covers
-  cout << "Amount of Covers: " << covers.size() << endl;
+  vector<string> mona =  printPowerSet(S, n);
 
-  for (auto elem : covers) {
-    cout << "Found Cover: " << elem << endl;
-  }
+  cout << "Amount of power sets: " << mona.size() << endl;
 
+  // *************************************************************************************************
+  // FINISHED POWER SET
+  // ****************************************************************************************************
 
   // Determine the total weight of each cover
-  for (auto elem : covers) {
+  for (auto elem : mona) {
     string tempCover = elem;
     istringstream iss(tempCover);
     vector<string> v;
@@ -117,19 +122,11 @@ int main(int argc, char const *argv[]) {
 
     int total = 0;
     vector<int> elemList;
-    cout << "Testing the following cover: ";
-    cout << tempCover << endl;
     for (int i = 0; i < v.size(); i++) {
-      // find total weight of cover
-      //string t = "";
-      //t += v.at(i);
-      //cout << "SubSet: " << v.at(i) << endl;
       stringstream toInt(v.at(i));
       int subSetNum;
       toInt >> subSetNum;
       total += _weight.at(subSetNum);
-      //cout << "Cost of subset: " << subSetNum << " : " << _weight.at(subSetNum) << endl;
-      // obtain the stored SubSet
       vector<int> tempSubSet = _set.at(_weight.at(subSetNum));
       // store each elem in the tempSubSet to uniqueElem to create a true cover
       for (int j = 0; j < tempSubSet.size(); j++) {
@@ -137,31 +134,12 @@ int main(int argc, char const *argv[]) {
       }
     }
 
-    // testing
-    cout << "Total weight: " << total << endl;
-    cout << "Amount of elements in cover: " << elemList.size() << endl;
-    cout << " --- " << endl;
 
-
-      // determine the best cover with the lowest weight
-      // update info when if statments are true
-      /*
-        minWeight -> lowest weight found
-        bestCover -> best cover found
-        bestCoverSize -> lenght of the best cover found
-      */
-      //cout << endl;
-      //cout << endl;
     if (elemList.size() <= numElements) {
       if (elemList.size() >= bestCoverSize) {
         //minWeight = total;
         bestCover = tempCover;
         bestCoverSize = elemList.size();
-
-        //cout << "Almost winner: " << endl;
-        //cout << "Weight total: " << minWeight << endl;
-        //cout << "Cover string: " << bestCover << endl;
-        //cout << "Amount of elements: " << bestCoverSize << endl;
 
         almostWin.push_back(tempCover);
       }
@@ -173,10 +151,12 @@ int main(int argc, char const *argv[]) {
 
   // finalist
   cout << "Amount of finalist: " << almostWin.size() << endl;
-  cout << endl;
+  //cout << endl;
   for (int i = 0; i < almostWin.size(); i++) {
     string tempCover = almostWin.at(i);
-    cout << "Final Cover: " << tempCover << endl;
+    //cout << "Final Cover: " << tempCover << endl;
+
+
 
     istringstream iss(tempCover);
     vector<string> v;
@@ -184,19 +164,17 @@ int main(int argc, char const *argv[]) {
     for (string s; iss >> s; ) {
       v.push_back(s);
     }
+
+
+    //
     int total = 0;
     vector<int> elemList;
     for (int i = 0; i < v.size(); i++) {
-      // find total weight of cover
-      //string t = "";
-      //t += v.at(i);
-      //cout << "SubSet: " << v.at(i) << endl;
       stringstream toInt(v.at(i));
       int subSetNum;
       toInt >> subSetNum;
       total += _weight.at(subSetNum);
 
-      //cout << "Cost of subset: " << subSetNum << " : " << _weight.at(subSetNum) << endl;
       // obtain the stored SubSet
 
         // obtain the stored SubSet
@@ -206,13 +184,24 @@ int main(int argc, char const *argv[]) {
             elemList.push_back(tempSubSet.at(j));
         }
 
-        if (elemList.size() == bestCoverSize) {
-          if (total <= minWeight) {
-            cout << "Winning cost: " << total << ". With cover: " << tempCover << endl;
-            minWeight = total;
-            bestCover = tempCover;
-          }
+        // check elemList for duplication
+        vector<string> check;
+        for (int k = 0; k < elemList.size(); k++){
+          string t = "";
+          t += elemList.at(k);
+          check.push_back(t);
         }
+        // TODO
+
+        //if(printDuplicates(check)) {
+          if (elemList.size() == bestCoverSize) {
+            if (total <= minWeight) {
+              cout << "Winning cost: " << total << ". With cover: " << tempCover << endl;
+              minWeight = total;
+              bestCover = tempCover;
+            }
+          }
+        //}
      }
   }
 
@@ -248,6 +237,56 @@ int main(int argc, char const *argv[]) {
   myfile.close();
   return 0;
 }
+
+
+char firstRepeating(string &str)
+{
+    // Creates an empty hashset
+    unordered_set<char> h;
+
+    // Traverse the input array from left to right
+    for (int i=0; i<str.length(); i++)
+    {
+        char c = str[i];
+
+        // If element is already in hash set, update x
+        // and then break
+        if (h.find(c) != h.end())
+            return c;
+
+        else // Else add element to hash set
+            h.insert(c);
+    }
+
+    // If there was no repeated character
+    return '\0';
+}
+
+bool printDuplicates(vector<string> words)
+{
+    vector<string>duplicate;
+
+    // STL function to sort the array of string
+    sort(words.begin(), words.end());
+
+    for (int i = 1; i<words.size(); i++)
+        if (words[i-1] == words[i])
+
+            // STL function to push the duplicate
+            // words in a new vector string
+            duplicate.push_back(words[i]);
+
+  if (duplicate.size() > 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+  return false;
+
+}
+
 
 // find covers, decrement
 void findDec (int numSets) {
@@ -295,4 +334,37 @@ vector<string> split(const string &s, char delim) {
     vector<string> elems;
     split(s, delim, back_inserter(elems));
     return elems;
+}
+
+
+vector<string> printPowerSet(vector<int> __set, int set_size)
+{
+    int n = 0;
+    vector<string> static_vector;
+    while ( n < 64 ) {
+
+      /*set_size of power set of a set with set_size
+      n is (2**n -1)*/
+      unsigned int pow_set_size = pow(2, set_size);
+      int counter, j;
+
+      /*Run from counter 000..0 to 111..1*/
+      for(counter = 0; counter < pow_set_size; counter++)
+      {
+        string TT = "";
+        for(j = 0; j < set_size; j++)
+        {
+            /* Check if jth bit in the counter is set
+                If set then print jth element from set */
+            if(counter & (1 << j))
+                TT += to_string(__set[j]) + " ";
+
+
+        }
+        cout << "Power Set Elem: " << TT << endl;
+        static_vector.push_back(TT);
+      }
+      n++;
+    }
+    return static_vector;
 }
